aac3bbe1a3e1cee324b799ed0d389eb5368e6802
tuan <tuan@vietswiss.com>
2020-04-11 16:19:07 +0700

    done

--- wp-vue-plugin-boilerplate.php
+++ wp-vue-plugin-boilerplate.php
@@ -18,13 +18,14 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 // TODO: replace PLUGIN_NAME with yours
 define( 'PLUGIN_NAME_FILE', __FILE__ );
 define( 'PLUGIN_NAME_VERSION', '0.1.0.0' );
+define( 'SCRIPT_NAME', 'network-zeit');
 
 require plugin_dir_path( PLUGIN_NAME_FILE ) . 'includes/shortcodes.php';
 
 add_action( 'wp_enqueue_scripts', function() {
   wp_register_script(
-    'rexer-components',
-    plugin_dir_url( PLUGIN_NAME_FILE ) . 'public/js/events-rexer.js',
+    SCRIPT_NAME,
+    plugin_dir_url( PLUGIN_NAME_FILE ) . 'public/js/'.(SCRIPT_NAME).'.js',
     array(),
     PLUGIN_NAME_VERSION,
     true
